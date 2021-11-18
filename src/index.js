e77198b29ef191a9bd36cd9a74fcf196ad03435d
VladimirKadochnikov <kvk0709@gmail.com>
2021-11-18 17:59:58 +0300

    HW_2

--- src/index.js
+++ src/index.js
@@ -1,20 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const MessageComponent = (props) => {
-  return (
-    <div>
-      <h2 className="test-message">{props.message}</h2>
+const MessageComponent = ({ messageList = [] }) => {
+  return messageList.map((message) => (
+    <div className="message">
+      <h2 className="name">{message.author}</h2>
+      <p className="text">{message.message}</p>
     </div>
-  );
+  ));
 };
 
 const App = () => {
+  const [messageList, setMessageList] = useState([]);
+  const [name, setName] = useState("");
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    const lastMess = messageList[messageList.length - 1];
+    if (messageList.length && lastMess.author !== "Bot") {
+      setMessageList([...messageList, { author: "Bot", message: `Hello` }]);
+    }
+  }, [messageList]);
+
+  const addMessage = () => {
+    if (name !== "" && text !== "") {
+      setMessageList([...messageList, { author: name, message: text }]);
+      setName("");
+      setText("");
+    }
+  };
+
+  const handleName = (event) => {
+    setName(event.target.value);
+  };
+  const handleText = (event) => {
+    setText(event.target.value);
+  };
+
   return (
     <div>
-      <h1 className="test-header">Вывод на экран:</h1>
-      <MessageComponent message="Тестовое сообщение" />
+      <h1 className="test-header">Сообщения пользователей:</h1>
+      <form action="#" className="form">
+        <input
+          className="form-name"
+          placeholder="Ваше имя"
+          onChange={handleName}
+          value={name}
+        />
+        <input
+          className="form-text"
+          placeholder="Сообщение"
+          onChange={handleText}
+          value={text}
+        />
+        <button className="form-button" onClick={addMessage}>
+          Отправить
+        </button>
+      </form>
+      <MessageComponent messageList={messageList} />
     </div>
   );
 };
